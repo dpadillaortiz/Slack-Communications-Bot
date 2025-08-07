483fbd8b3765415069f16fdb859d5564830ee8ce
twinky <padillaortiz.daniel099@gmail.com>
2025-08-07 01:47:44 -0700

    experimenting for async slack_bolt

--- send_message.py
+++ send_message.py
@@ -3,7 +3,12 @@ import json
 
 from slack_bolt import App
 from slack_sdk.errors import SlackApiError
-from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+
+import asyncio
+from slack_bolt.async_app import AsyncApp
+from slack_sdk.errors import SlackApiError
+from slack_sdk.web.async_client import AsyncWebClient
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -23,13 +28,30 @@ app = App(
     signing_secret=SLACK_SIGNING_SECRET
 )
 
+async_app = AsyncApp(
+    token=SLACK_BOT_TOKEN,
+    signing_secret=SLACK_SIGNING_SECRET
+)
+
+
 def get_block_message():
     with open("block_kit.json",'r') as file:
         blocks = json.load(file)["blocks"]
     return json.dumps(blocks)
 
-app.client.chat_postMessage(
-    channel=CHANNEL_ID,
-    blocks=get_block_message(),
-    text="Message from Endpoint Engineering"
-)
+
+provided_emails=["hazrd510@gmail.com", "padillaortiz.daniel099@gmail.com"]
+
+async def send_windows_message(email: str):
+    try:
+        user_id = await async_app.client.users_lookupByEmail(email=email)["user"]["id"]
+        await async_app.client.chat_postMessage(
+            channel=user_id,
+            blocks=get_block_message(),
+            text="Message from Endpoint Engineering"
+        )
+    except SlackApiError as e:
+        print(f"Failed for {email}: {e.response['error']}")
+
+async def message_multiple_users(emails: list[str]):
+    await asyncio.gather(*(send_windows_message(email) for email in emails))
