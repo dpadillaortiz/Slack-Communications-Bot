9da29a61e0fb5f24bdbde785424c6cca8befd1c3
twinky <padillaortiz.daniel099@gmail.com>
2025-08-10 23:31:19 -0700

    copied code over from socket_mode to main

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ load_dotenv()
 
 SLACK_SIGNING_SECRET = os.getenv("SLACK_SIGNING_SECRET")
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+SLACK_CANVAS = os.getenv("SLACK_CANVAS")
+TENTATIVE_SECTION=os.getenv("TENTATIVE_SECTION")
+ALT_SECTION_1=os.getenv("ALT_SECTION_1")
+ALT_SECTION_2=os.getenv("ALT_SECTION_2")
+ALT_SECTION_3=os.getenv("ALT_SECTION_3")
 
 # Initializes your app with your bot token and signing secret
 # https://api.slack.com/authentication/verifying-requests-from-slack
@@ -33,7 +38,8 @@ def handle_confirm_tentative(client, body, logger):
         private_metadata={
             "date":body["actions"][0]["value"],
             "message_ts":body["message"]["ts"],
-            "user_id":body["container"]["channel_id"]
+            "user_id":body["container"]["channel_id"],
+            "caller_id":body["message"]["user"]
         }
         confirmation_message=f':spiral_calendar_pad: I am scheduling my Windows upgrade on *{body["actions"][0]["value"]}*.'
         client.views_open(
@@ -53,7 +59,8 @@ def handle_alternative_choice(body, client, logger):
         private_metadata = {
             "date": selected_date,
             "message_ts": body["message"]["ts"],
-            "user_id": body["container"]["channel_id"]
+            "user_id": body["container"]["channel_id"],
+            "caller_id":body["message"]["user"]
         }
         confirmation_message = f":spiral_calendar_pad: I am scheduling my Windows upgrade on *{selected_date}*"
         client.views_open(
@@ -80,6 +87,34 @@ def handle_view_submission_events(body, client, logger):
             ts=private_metadata["message_ts"],
             text=f"You have selected the week of {private_metadata['date']} for your {private_metadata['windows_version']} upgrade."
         )
+        if private_metadata['date'] == private_metadata["tentative_schedule"]:
+            client.canvases_edit(
+                canvas_id=SLACK_CANVAS,
+                changes=[{"operation":"insert_after",
+                        "document_content":{"type":"markdown","markdown":f"* {private_metadata['user_email']}"},
+                        "section_id":f"{TENTATIVE_SECTION}"}]
+            )
+        elif private_metadata['date'] == private_metadata["alternate_schedule_1"]:
+            client.canvases_edit(
+                canvas_id=SLACK_CANVAS,
+                changes=[{"operation":"insert_after",
+                        "document_content":{"type":"markdown","markdown":f"* {private_metadata['user_email']}"},
+                        "section_id":f"{ALT_SECTION_1}"}]
+            )
+        elif private_metadata['date'] == private_metadata["alternate_schedule_2"]:
+            client.canvases_edit(
+                canvas_id=SLACK_CANVAS,
+                changes=[{"operation":"insert_after",
+                        "document_content":{"type":"markdown","markdown":f"* {private_metadata['user_email']}"},
+                        "section_id":f"{ALT_SECTION_2}"}]
+            )
+        elif private_metadata['date'] == private_metadata["alternate_schedule_3"]:
+            client.canvases_edit(
+                canvas_id=SLACK_CANVAS,
+                changes=[{"operation":"insert_after",
+                        "document_content":{"type":"markdown","markdown":f"* {private_metadata['user_email']}"},
+                        "section_id":f"{ALT_SECTION_3}"}]
+            )
     except SlackApiError as e:
         logger.error(f"Failed to open modal: {e}")
 app.view("confirmation_view")(ack=respond_to_slack_within_3_seconds, lazy=[handle_view_submission_events])
@@ -103,6 +138,7 @@ def send_windows_message(client, email: str, schedules: dict[str:str], windows_v
             blocks=ui_templates.build_blocks_message(schedules, windows_version),
             text="Message from Endpoint Engineering"
         )
+        ui_templates.update_confirmation_template({"user_email":email})
     except SlackApiError as e:
         print(f"Failed for {email}: {e.response['error']}")
 
@@ -114,7 +150,6 @@ def handle_shortcut_submission_events(ack, body, client, logger, view):
     ack()
     logger.info(body)
     windows_version=view["state"]["values"]["windows_version"]["windows_version-action"]["value"]
-    ui_templates.update_confirmation_template({"windows_version":windows_version})
     provided_emails=view["state"]["values"]["provided_emails"]["provided_emails-action"]["value"].split(",")
     provided_schedules={
         "windows_version":view["state"]["values"]["windows_version"]["windows_version-action"]["value"],
@@ -123,6 +158,7 @@ def handle_shortcut_submission_events(ack, body, client, logger, view):
         "alternate_schedule_2": view["state"]["values"]["alternate_schedule_2"]["alternate_schedule_2-action"]["value"],
         "alternate_schedule_3": view["state"]["values"]["alternate_schedule_3"]["alternate_schedule_3-action"]["value"]
     }
+    ui_templates.update_confirmation_template(provided_schedules)
     message_multiple_users(client, provided_emails, provided_schedules, windows_version)
 
 app.view("windows_update_modal_view")(ack=respond_to_slack_within_3_seconds, lazy=[handle_shortcut_submission_events])
